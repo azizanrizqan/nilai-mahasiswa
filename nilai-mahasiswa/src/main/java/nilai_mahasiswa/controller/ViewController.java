1ee000ade4eb4f033f849174507ba38d19f9005b
Azizan Rizqan <azizanrizqan.ar@gmail.com>
2026-06-11 18:57:31 +0700

    CRUD frontend lengkap

--- nilai-mahasiswa/src/main/java/nilai_mahasiswa/controller/ViewController.java
+++ nilai-mahasiswa/src/main/java/nilai_mahasiswa/controller/ViewController.java
@@ -1,9 +1,10 @@
 package nilai_mahasiswa.controller;
 
+import nilai_mahasiswa.entity.Mahasiswa;
 import nilai_mahasiswa.service.MahasiswaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ViewController {
@@ -18,5 +19,54 @@ public class ViewController {
     public String home(Model model) {
         model.addAttribute("listMahasiswa", service.findAll());
         return "index";
+    }
+
+    @GetMapping("/tambah")
+    public String tambahForm(Model model) {
+
+        model.addAttribute(
+                "mahasiswa",
+                new Mahasiswa()
+        );
+
+        return "form";
+    }
+
+    @PostMapping("/simpan")
+    public String simpan(
+            @ModelAttribute Mahasiswa mahasiswa) {
+
+        service.save(mahasiswa);
+
+        return "redirect:/";
+    }
+    @GetMapping("/hapus/{id}")
+    public String hapus(@PathVariable Long id) {
+
+        service.delete(id);
+
+        return "redirect:/";
+    }
+    @GetMapping("/edit/{id}")
+    public String editForm(
+        @PathVariable Long id,
+        Model model) {
+
+        model.addAttribute(
+            "mahasiswa",
+            service.findById(id)
+     );
+
+        return "edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(
+        @PathVariable Long id,
+        @ModelAttribute Mahasiswa mahasiswa) {
+
+        service.update(id, mahasiswa);
+
+        return "redirect:/";
     }
 }
